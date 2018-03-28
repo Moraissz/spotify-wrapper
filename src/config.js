e87a7c81926a03067ed7b6bbac7f1d6c3ca6b8ab
Lucas Morais <lucas.moraissz07@gmail.com>
2018-03-27 21:47:12 -0300

    Adding a new token and updating README.md

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://api.spotify.com/v1/';
-const TOKEN_API = 'BQBVMDlsPPn2VssQWE4pWtBoTlGHGJFT7dpNunTfTEQjfy5qHg9D4FRDSk6cjCmqmvwS9iDzcMydyVFOtwjfxboZgBILxzQ6AIgB-ucT5_Rd1wamyfPeLBLbxGhVJp4-jGc6wQML';
+const TOKEN_API = 'BQCQnWUXu0JKn5AA8ROK2bfQ6j8C1O8zTnqarlO9g81Cg9GuIKTpycyAH7ti8R2u_vLLyl5i0z8njudHlJAMh3iDNEN1QIfEuB4wYnDZ1doAoJ007Or1fb6y8BlXu0Xx9dXUTmfE';
 const headers = {
   headers: {
     Authorization: `'Bearer ${TOKEN_API}'`,
